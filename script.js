2b026054813851b338218fca238bd9268c0ab9ad
abdulazizcreator <abdulazizprogrammer@gmail.com>
2022-12-19 18:42:13 +0500

    checkbox

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-const body = document.querySelector("body"),
-      modeToggle = body.querySelector(".mode-toggle");
-      sidebar = body.querySelector("nav");
-      sidebarToggle = body.querySelector(".sidebar-toggle");
+const body = document.querySelector("body");
+const modeToggle = body.querySelector(".mode-toggle");
+const sidebar = body.querySelector("nav");
+const sidebarToggle = body.querySelector(".sidebar-toggle");
 
-let getMode = localStorage.getItem("mode");
-if(getMode && getMode ==="dark"){
-    body.classList.toggle("dark");
-}
+// let getMode = localStorage.getItem("mode");
+// if (getMode && getMode === "dark") {
+//   body.classList.toggle("dark");
+// }
 
-let getStatus = localStorage.getItem("status");
-if(getStatus && getStatus ==="close"){
-    sidebar.classList.toggle("close");
-}
+// let getStatus = localStorage.getItem("status");
+// if (getStatus && getStatus === "close") {
+//   sidebar.classList.toggle("close");
+// }
 
-modeToggle.addEventListener("click", () =>{
-    body.classList.toggle("dark");
-    if(body.classList.contains("dark")){
-        localStorage.setItem("mode", "dark");
-    }else{
-        localStorage.setItem("mode", "light");
-    }
+modeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  //   if (body.classList.contains("dark")) {
+  //     localStorage.setItem("mode", "dark");
+  //   } else {
+  //     localStorage.setItem("mode", "light");
+  //   }
 });
 
 sidebarToggle.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-    if(sidebar.classList.contains("close")){
-        localStorage.setItem("status", "close");
-    }else{
-        localStorage.setItem("status", "open");
-    }
-})
+  sidebar.classList.toggle("close");
+  //   if (sidebar.classList.contains("close")) {
+  //     localStorage.setItem("status", "close");
+  //   } else {
+  //     localStorage.setItem("status", "open");
+  //   }
+});
